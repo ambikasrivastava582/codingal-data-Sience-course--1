66c1be22ba82a342adf1efb8d06c355190277bec
ambikasrivastava582 <ambikasrivastava582@gmail.com>
2026-06-27 13:37:41 +0530

    Add files via upload

---
+++ filehand_1_notes_cleaner.py
@@ -0,0 +1,20 @@
+# read all lines into a list
+file = open('class-notes.txt', 'r')
+lines = file.readlines()
+file.close()
+
+# open a new file for writing
+out = open('odd-lines.txt', 'w')
+
+# range(0, len, 2) gives index 0, 2, 4, 6 -- every other line
+for i in range(0, len(lines), 2):
+    out.write(lines[i])
+
+out.close()
+print('Odd lines saved to odd-lines.txt')
+
+# odd-lines.txt will contain:
+# Maths: Learn equations today
+# Science: Photosynthesis
+# English: Essay writing
+# History: World War II
